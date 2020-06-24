5e9fad565a8a538c3ecb95a35c6c259cae712622
Juwendays <juwendays@gmail.com>
2020-06-24 11:23:01 +0700

    beljar react di web react

--- src/index.js
+++ src/index.js
@@ -11,6 +11,18 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const name = "juwendays";
+
+
+function kontol(){
+
+  return "Jancok " + name
+}
+const element = <h1> Hello {kontol(name)} !!</h1>;
+
+ReactDOM.render(element,document.getElementById('master'));
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
